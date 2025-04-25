c833ee147901a6a2c2e2e1dc99e653ee5a0adece
Nikhil Pareek <nikhilpareekpandit@gmail.com>
2025-04-26 02:19:40 +0530

    Deploy Flutter web preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:currency/binaryMaterial.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => MyApp(),
+    ),
+  );
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // ignore: deprecated_member_use
+      useInheritedMediaQuery: true, // 🛠 REQUIRED for DevicePreview
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
-      home: Binarymaterial()
+      home: Binarymaterial(),
     );
   }
 }
+
